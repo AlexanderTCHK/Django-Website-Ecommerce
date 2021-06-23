7ff28065e91a4f5eac4a348ccf8be5b6ac224f9e
unknown <C:\Users\Home\AppData\Roaming\The Bat!>
2021-06-23 18:58:17 +0300

    Adding functionality for non logged user via Cookies

--- shop/views.py
+++ shop/views.py
@@ -3,49 +3,31 @@ from django.http import JsonResponse
 import json
 import datetime
 from .models import *
+from .utils import cookieCart, cart_list_data
 
 
 def shop(request):
-
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
-        cartItems = order['get_cart_items']
-
+    data = cart_list_data(request)
+    cartItems = data['cartItems']
+    
     products = Product.objects.all()
     context = {"products": products, 'cartItems': cartItems}
     return render(request, 'shop/shop.html', context)
 
 def cart_list(request):
-
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
-        cartItems = order['get_cart_items']
+    data = cart_list_data(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'shop/cart_list.html', context)
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
-        cartItems = order['get_cart_items']
+    data = cart_list_data(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems,'shipping':False}
     return render(request, 'shop/checkout.html', context)
